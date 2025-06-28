1270143fb4b5e862c17f0da8df85e698d7273407
najeebnaju512 <najeebnaju512@gmail.com>
2025-06-28 19:20:13 +0530

    Add internet permission to AndroidManifest and comment out debugPrint statements in print.dart
    - Added the INTERNET permission to the AndroidManifest.xml to enable network access for the application.
    - Commented out debugPrint statements in print.dart to prevent debug output during development, improving performance and reducing clutter in logs.

--- lib/core/dev_tools/print.dart
+++ lib/core/dev_tools/print.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void devPrint(String text, {PrintType type = PrintType.normal}) {
   switch (type) {
     case PrintType.normal:
-      debugPrint(text);
+      // debugPrint(text);
       break;
     case PrintType.warning:
       devPrintWarning(text);
@@ -18,15 +18,15 @@ void devPrint(String text, {PrintType type = PrintType.normal}) {
 }
 
 void devPrintWarning(String text) {
-  debugPrint('\x1B[33m$text\x1B[0m');
+  // debugPrint('\x1B[33m$text\x1B[0m');
 }
 
 void devPrintError(String text) {
-  debugPrint('\x1B[31m$text\x1B[0m');
+  // debugPrint('\x1B[31m$text\x1B[0m');
 }
 
 void devPrintSuccess(String text) {
-  debugPrint('\x1B[32m$text\x1B[0m');
+  // debugPrint('\x1B[32m$text\x1B[0m');
 }
 
 enum PrintType { normal, error, warning, success }
